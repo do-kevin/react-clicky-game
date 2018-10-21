0467f105a46502504e0f2eb54499dc84d351b8d3
Kevin Do <do.kevin.vo@gmail.com>
2018-10-20 19:36:47 -0700

    Updates

---
+++ precache-manifest.5990680a4118b4c41e3c1c0ac377fa77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "55abe30a270b3be75741",
+    "url": "/react-clicky-game/static/js/runtime~main.55abe30a.js"
+  },
+  {
+    "revision": "2f9fa54f19349ee7d87f",
+    "url": "/react-clicky-game/static/js/main.2f9fa54f.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/react-clicky-game/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "2f9fa54f19349ee7d87f",
+    "url": "/react-clicky-game/static/css/main.8a43eb9f.chunk.css"
+  },
+  {
+    "revision": "b28b068321551a341b54ec666b65ce01",
+    "url": "/react-clicky-game/index.html"
+  }
+];
