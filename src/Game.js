406f101ffe9acc6e1e8f084a1cb983cafe1a367a
Kevin Do <do.kevin.vo@gmail.com>
2018-10-21 15:25:29 -0700

    Sets up initial components

---
+++ src/Game.js
@@ -0,0 +1,14 @@
+import React , { Component } from "react";
+import Score from "./components/Score";
+import pokemons from "./pokemons.json"
+
+class Game extends Component {
+    render() {
+        return (
+
+        );
+    };
+}
+
+
+export default Game;
---
+++ src/components/Score.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Score = props => {
+    console.log(props);
+    <p class="nav-item">SCORE: {props}</p>;
+}
+
+export default Score;
