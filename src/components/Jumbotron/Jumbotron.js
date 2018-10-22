e378aeb0ee305aec4b18bd24edddd6b05962512e
Kevin Do <do.kevin.vo@gmail.com>
2018-10-21 18:28:40 -0700

    Sets up Jumbotron component

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = function() {
+  return (
+    <div className="jumbotron">
+      <h1>Remember Those Pokemons!</h1>
+      <h2>Click on a Pokemon, but remember to not click on the same Pokemon twice!</h2>
+    </div>
+  );
+};
+
+export default Jumbotron;
