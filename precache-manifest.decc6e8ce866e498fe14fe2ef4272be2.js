8ce808ff5037df25fe1c5e6159b72d0b77de2554
Kevin Do <do.kevin.vo@gmail.com>
2018-10-21 23:57:48 -0700

    Updates

---
+++ precache-manifest.decc6e8ce866e498fe14fe2ef4272be2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e83295a144a58e26df247df78c1a816",
+    "url": "/react-clicky-game/static/media/snorlax-bg.4e83295a.jpg"
+  },
+  {
+    "revision": "d250deeaf60b5ecebfd83093b7df5a0b",
+    "url": "/react-clicky-game/static/media/just-waves.d250deea.png"
+  },
+  {
+    "revision": "55abe30a270b3be75741",
+    "url": "/react-clicky-game/static/js/runtime~main.55abe30a.js"
+  },
+  {
+    "revision": "687607404f7045e81119",
+    "url": "/react-clicky-game/static/js/main.68760740.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/react-clicky-game/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "687607404f7045e81119",
+    "url": "/react-clicky-game/static/css/main.7c0b0bcc.chunk.css"
+  },
+  {
+    "revision": "302204297f4f9981cfa14c7f1cad844d",
+    "url": "/react-clicky-game/index.html"
+  }
+];
